bc560a7217e64782bdd04c061e4976eaef11d7b7
Akbar Nafisa <akbarnj788@gmail.com>
2024-01-22 11:23:28 +0700

    feat: add new api

--- app_1/main.js
+++ app_1/main.js
@@ -6,11 +6,18 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (_, res) => {
+app.get('/app_1', (_, res) => {
   res.send({
     data: 'hello from BE 1'
   })
 })
+
+app.get('/app_1/test', (_, res) => {
+  res.send({
+    data: 'hello from BE 1 Test'
+  })
+})
+
 
 app.listen(3131, () => {
   console.log(`listening to port http://localhost:3131`)
--- app_2/main.js
+++ app_2/main.js
@@ -7,11 +7,19 @@ app.use(express.json())
 app.use(cors())
 
 
-app.get('/', (_, res) => {
+app.get('/app_2', (_, res) => {
   res.send({
     data: 'hello from BE 2'
   })
 })
+
+app.get('/app_2/test', (_, res) => {
+  res.send({
+    data: 'hello from BE 2 Test'
+  })
+})
+
+
 
 app.listen(3232, () => {
   console.log(`listening to port http://localhost:3232`)
